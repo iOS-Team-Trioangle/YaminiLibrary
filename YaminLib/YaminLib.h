212420cbfca516f3333ff3309ca4c1206b600e4e
sivasonai <sivasonai@trioangle.com>
2021-10-26 16:05:00 +0530

    Inital Project Setup -> Karuppasamy

---
+++ YaminLib/YaminLib.h
@@ -0,0 +1,18 @@
+//
+//  YaminLib.h
+//  YaminLib
+//
+//  Created by Trioangle on 26/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YaminLib.
+FOUNDATION_EXPORT double YaminLibVersionNumber;
+
+//! Project version string for YaminLib.
+FOUNDATION_EXPORT const unsigned char YaminLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YaminLib/PublicHeader.h>
+
+
