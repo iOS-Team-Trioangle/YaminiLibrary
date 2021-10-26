b2a97299bca6e6ff24ae8a209d55d62cddc89cb1
sivasonai <sivasonai@trioangle.com>
2021-10-26 16:31:29 +0530

    Test Commit

---
+++ YaminLib/TestFile.swift
@@ -0,0 +1,17 @@
+//
+//  TestFile.swift
+//  YaminLib
+//
+//  Created by Trioangle on 26/10/21.
+//
+
+import Foundation
+
+open
+class Welcome {
+    
+    public
+    func createToast() {
+        print("Welcome")
+    }
+}
